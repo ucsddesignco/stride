87e305832fef9f508457a79108b649a2ab4eb719
Emily Nguyen <emilymylinh@gmail.com>
2019-01-07 22:08:48 -0800

    Add modals

--- _site/assets/scripts/modal.js
+++ _site/assets/scripts/modal.js
@@ -1,5 +1,23 @@
-$(document).ready(function () {
-  $(".company-card").on("click", function () {
-      $(this).next().fadeIn("fast");
+$(document).ready(function() {
+  // Truncate descriptions if necessary
+  $(".card-description").each(function() {
+    const limit = 250;
+    let text = $(this).text();
+    if (text.length > limit) {
+      text = text.substring(0, limit) + "...";
+      $(this).text(text);
+    }
+  });
+
+  $(".company-card").on("click", function() {
+    $(this)
+      .next()
+      .fadeIn("fast");
+  });
+
+  $(".close-modal-btn").on("click", function() {
+    $(this)
+      .closest(".modal-overlay")
+      .fadeOut("fast");
   });
 });
